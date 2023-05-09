52080330c705471405bb1542c42e6548b413c172
IamAnkitR <ardb8755@gmail.com>
2023-05-10 00:07:28 +0530

    new layout

---
+++ src/components/Pathfinding/Node.js
@@ -0,0 +1,33 @@
+import React, { Component } from "react";
+
+class Node extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+
+        }
+    }
+    render() {
+        const {
+            value,
+            key,
+            onMouseDown,
+            row,
+            col,
+            onMouseEnter,
+            onMouseUp,
+            onMouseLeave
+        } = this.props;
+        const cName = value === 5 ? "start" : value === 10 ? "end" : value === 1000 ? "wall" : "";
+        return (
+            <td className={"node_" + cName}
+                id={`node-${row}-${col}`}
+                onMouseDown={() => onMouseDown(row, col)}
+                onMouseEnter={() => onMouseEnter(row, col)}
+                onMouseUp={() => onMouseUp()}
+                onMouseLeave={() => onMouseLeave(row, col)}
+            ></td>
+        )
+    }
+}
+export default Node;
